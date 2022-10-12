4f2cfedacc5568f46f3832aa05988fb47de72ea7
simis <simi.popola@gmail.com>
2022-10-12 21:41:57 +0100

    JUnit tests for listing notes from collection

--- src/main/kotlin/controllers/NoteAPI.kt
+++ src/main/kotlin/controllers/NoteAPI.kt
@@ -4,16 +4,16 @@ import models.Note
 class NoteAPI {
     private var notes = ArrayList<Note>()
 
-    fun add(note: Note): Boolean{
+    fun add(note: Note): Boolean {
         return notes.add(note)
     }
 
     fun listAllNotes(): String {
-        return if (notes.isEmpty()){
+        return if (notes.isEmpty()) {
             "No notes stored"
-        }else {
+        } else {
             var listOfNotes = ""
-            for (i in notes.indices){
+            for (i in notes.indices) {
                 listOfNotes += "${i}: ${notes[i]} \n"
             }
             listOfNotes
@@ -24,13 +24,12 @@ class NoteAPI {
         return notes.size
     }
 
-    fun findNote(index: Int): Note?{
-        return if (isValidListIndex(index, notes)){
+    fun findNote(index: Int): Note? {
+        return if (isValidListIndex(index, notes)) {
             notes[index]
-        }else null
-    }
-
-    fun isValidListIndex(index: Int, list: List<Any>): Boolean {
-        return (index >= 0 &&index < list.size)
+        } else null
     }
 }
+    fun isValidListIndex(index: Int, list: List<Any>): Boolean {
+        return (index >= 0 && index < list.size)
+    }
